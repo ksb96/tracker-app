5d735a7de173e6a4c3b44d111c110783a65db651
kubstar <52975077+kubstar@users.noreply.github.com>
2023-02-25 16:00:07 +0530

    new changes --added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from "../../services/ui.service";
 import { Subscription } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -12,11 +13,17 @@ export class HeaderComponent {
   showAddTask: boolean = false;
   subscription!: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
   }
 
+  //methods
   toggleAddtask(): void {
     this.uiService.toggleAddTask();
+  }
+
+  //has access to 'ui-service component' & will show only on 'index(/)'
+  hasRoute(route: String) {
+    return this.router.url === route;
   }
 }
